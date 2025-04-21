9c1a2d33a07c4bb31335825da098a4c78d786b66
CMS37 <rhtn037@naver.com>
2025-04-21 15:08:36 +0900

    불필요 파일 첨삭삭

--- 00_config.js
+++
@@ -1,6 +0,0 @@
-const Config = {
-	INS_ROOT: "https://ensembledata.com/apis",
-	API_TOKEN: PropertiesService.getScriptProperties()("API_TOKEN"),
-	TZ: Session.getScriptTimeZone(),
-	DATE_FMT: "yyyy-MM-dd'T'HH:mm:ss'Z'"
-};
